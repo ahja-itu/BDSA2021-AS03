8057936db7a6da7a3a46be4817b25433aaeeb26f
Andreas Wachs Hjalager <ahja@itu.dk>
2021-09-24 13:41:38 +0200

    Implement and test method IsSecure

--- Assignment3.Tests/ExtensionsTests.cs
+++ Assignment3.Tests/ExtensionsTests.cs
@@ -1,14 +1,39 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
-namespace BDSA2020.Assignment02.Tests
+
+namespace BDSA2021.Assignment03.Tests
 {
     public class ExtensionsTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [ClassData(typeof(IsSecureData))]
+        public void IsSecure_givenCustomDataSet_ShouldHoldForAll(string url, bool expected)
         {
+            Assert.Equal(expected, (new Uri(url)).IsSecure());
+        }
+        
+    }
 
+
+
+
+    /* 
+    
+
+    Test data class
+    
+    */
+
+
+    public class IsSecureData : TheoryData<string, bool>
+    {
+        public IsSecureData()
+        {
+            Add("http://mydomain.com", false);
+            Add("https://otherdomain.com", true);
         }
     }
 }
+
--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,8 +1,12 @@
 using System;
-
+using System.Linq;
 namespace BDSA2021.Assignment03
 {
     public static class Extensions
     {
+        public static bool IsSecure(this Uri uri)
+        {
+            return uri.ToString().StartsWith("https");
+        }
     }
 }
